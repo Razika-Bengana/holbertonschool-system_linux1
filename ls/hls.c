4cdcbc924030ce2b8d099fa33a5a8fea66495b95
Razika-Bengana <5003@holbertonstudents.com>
2023-03-21 12:57:57 +0100

    List the content of the current directory with hls

--- ls/hls.c
+++ ls/hls.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
+#include "hls.h"
+
 
 /**
  * main - Main function
  * Lists the content of the current directory by using hls
+ * @argc: argument count (the number of command line arguments)
+ * @argv: argument vector (an array of command line argument strings)
  * Return: 0 (success)
  */
-int main(void)
+
+int main(int argc, char *argv[])
 {
-
 	DIR *dir;
-
-	/* Directory entry */
 	struct dirent *ent;
 
-	/* Open the current working directory */
-	dir = opendir(".");
-	if (dir != NULL)
+	/* Check if a directory was specified as a command-line argument */
+	if (argc == 2)
 	{
-/* Loop through all the entries in the directory */
-		while ((ent = readdir(dir)) != NULL)
+		/* Open the specified directory */
+		dir = opendir(argv[1]);
+		if (dir == NULL)
 		{
-			if (ent->d_name[0] != '.')
-				printf("%s\t", ent->d_name);
+			fprintf(stderr, "Cannot open directory: %s\n", argv[1]);
+			return (EXIT_FAILURE);
 		}
-		printf("\n");
-/* Close the directory */
-		closedir(dir);
 	}
 	else
 	{
-/* Print an error message in case of fail */
-		perror("");
-		return (1);
+		/* Open the current working directory */
+		dir = opendir(".");
+		if (dir == NULL)
+		{
+			fprintf(stderr, "Cannot open directory\n");
+			return (EXIT_FAILURE);
+		}
 	}
-	return (0);
+
+	/* Loop through all the entries in the directory */
+	while ((ent = readdir(dir)) != NULL)
+	{
+		if (ent->d_name[0] != '.')
+		{
+			printf("%s\t", ent->d_name);
+		}
+	}
+	printf("\n");
+
+	/* Close the directory */
+	closedir(dir);
+
+	return (EXIT_SUCCESS);
 }
