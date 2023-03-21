159b0cad23b91d05e9b5fac0b501ae584af6347c
Razika-Bengana <5003@holbertonstudents.com>
2023-03-21 01:50:57 +0100

    Create a program that lists the content of the current directory with hls

--- ls/hls.c
+++ ls/hls.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main(void) {
+/**
+ * main - Main function
+ * Lists the content of the current directory by using hls
+ * Return : 0 (success)
+ */
+int main(void)
+{
 
 	DIR *dir;
 
@@ -9,22 +15,24 @@ int main(void) {
 	struct dirent *ent;
 
 	/* Open the current working directory */
-	if((dir = opendir(".")) != NULL)
+	dir = opendir(".");
+	if (dir != NULL)
 	{
-		/* Loop through all the entries in the directory */
+/* Loop through all the entries in the directory */
 		while ((ent = readdir(dir)) != NULL)
 		{
-			printf("%s\n", ent->d_name);
+			if (ent->d_name[0] != '.')
+				printf("%s\t", ent->d_name);
 		}
-		/* Close the directory */
+		printf("\n");
+/* Close the directory */
 		closedir(dir);
 	}
 	else
 	{
-		/* Print an error message in case of fail */
+/* Print an error message in case of fail */
 		perror("");
-		return 1;
+		return (1);
 	}
-
-	return 0;
+	return (0);
 }
