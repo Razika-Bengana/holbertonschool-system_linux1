bfc9de277257070eb91a0c029b844b43645dcbb7
Razika-Bengana <5003@holbertonstudents.com>
2023-03-29 05:24:11 +0200

    Racing cars

--- getline/laps.c
+++ getline/laps.c
@@ -1,149 +1,72 @@
 #include "laps.h"
 
-#define MAX_CARS 100
 
 /**
- * get_race - function that returns a pointer to a Race struct
- *
- * Return: a pointer to a Race struct
+ * add_car - function that adds new car to the race
+ * @head: a pointer to a pointer to the head node
+ * @id: the id of the car to be added
+ * return: nothing (void)
  */
-
-Race *get_race()
+void add_car(Car **head, int id)
 {
-/* declare static variable to persist between calls */
-	static Race *race;
+	Car *node, *temp;
 
-/*
- * if race struct has not been initialized yet,
- * allocate memory and set number of cars to 0
-*/
-	if (race == NULL)
+	if (!*head || id < (*head)->id)
 	{
-		race = malloc(sizeof(Race));
-		race->num_cars = 0;
+		node = malloc(sizeof(Car));
+		if (!node)
+			exit(EXIT_FAILURE);
+		node->id = id;
+		node->laps = 0;
+		node->next = *head;
+		*head = node;
+		printf("Car %d joined the race\n", id);
+		return;
 	}
-	return (race);
-}
-
-
-/**
- * find_car - function that searches for a car with the given id
- * @id: id of the car to find
- * Return: the index of the car in the race's array of cars,
- * or -1 if it is not found
- */
-
-int find_car(int id)
-{
-/* get the race struct pointer */
-	Race *race = get_race();
-	int i;
-
-/* if race struct has not been initialized, return -1 */
-	if (race == NULL)
+	for (node = *head; node->next && node->next->id <= id; node = node->next)
+		;
+	if (node->id == id)
 	{
-		return (-1);
+		node->laps++;
+		return;
 	}
-
-/* search through the array of cars for a car with the given id */
-	for (i = 0; i < race->num_cars; i++)
-	{
-		if (race->cars[i].id == id)
-		{
-			return (i);
-		}
-	}
-/* if car with given id is not found, return -1 */
-	return (-1);
-}
-
-
-/**
- * add_car - function that adds a car to the race's array of cars
- * with the specified id
- * @id: id of the car to add
- * @race: pointer to the race struct
- * Return: nothing (void)
- */
-
-void add_car(int id, Race *race)
-{
-/* add car with specified id to the array of cars in the race */
-	race->cars[race->num_cars].id = id;
-	race->cars[race->num_cars].laps = 0;
-	race->num_cars++;
+	temp = malloc(sizeof(Car));
+	if (!temp)
+		exit(EXIT_FAILURE);
+	temp->id = id;
+	temp->laps = 0;
+	temp->next = node->next;
+	node->next = temp;
 	printf("Car %d joined the race\n", id);
 }
 
 /**
- * sort_cars - function that sorts the cars in the race's array of cars
- * by their id
- * @race: pointer to the race struct
- * Return: nothing (void)
+ * race_state - function that prints the current state of the race
+ * @id: an integer array of car ids
+ * @size: the size of the integer array
+ * return: nothing (void)
  */
-
-void sort_cars(Race *race)
-{
-	int i, j;
-/* sort the cars in the array of cars in the race by their id */
-	for (i = 0; i < race->num_cars - 1; i++)
-	{
-		for (j = i + 1; j < race->num_cars; j++)
-		{
-			if (race->cars[i].id > race->cars[j].id)
-			{
-				Car temp = race->cars[i];
-
-				race->cars[i] = race->cars[j];
-				race->cars[j] = temp;
-			}
-		}
-	}
-}
-
-/**
-  * race_state - function that updates the number of laps for each car
- * in the race, sorts the cars by their id, and prints the current race state
- * @id: pointer to an array of car ids
- * @size: size of the array of car ids
- * Return: nothing (void)
- */
-
 void race_state(int *id, size_t size)
 {
-/* get the race struct pointer */
-	Race *race = get_race();
-	int i;
+	static Car *head;
+	Car *node;
+	size_t i;
 
-/* if size of car id array is 0, free allocated memory and return */
-	if (size == 0)
+	if (!size)
 	{
-/* free allocated memory */
-		race->num_cars = 0;
+		while (head)
+		{
+			node = head;
+			head = head->next;
+			free(node);
+		}
 		return;
 	}
-
-	for (i = 0; i < (int) size; i++)
-	{
-		int car_idx = find_car(id[i]);
-
-		if (car_idx == -1)
-		{
-			add_car(id[i], race);
-		}
-		else
-		{
-			race->cars[car_idx].laps++;
-		}
-	}
-
-	sort_cars(race);
-
+	for (i = 0; i < size; i++)
+		add_car(&head, id[i]);
 
 	printf("Race state:\n");
+	for (node = head; node; node = node->next)
+		printf("Car %d [%lu laps]\n", node->id, node->laps);
 
-	for (i = 0; i < race->num_cars; i++)
-	{
-		printf("Car %d [%d laps]\n", race->cars[i].id, race->cars[i].laps);
-	}
 }
--- getline/laps.h
+++ getline/laps.h
@@ -1,43 +1,23 @@
-#ifndef LAPS_H_
-#define LAPS_H_
+#ifndef LAPS_H
+#define LAPS_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-
-#define MAX_CARS 100
+#include <stdio.h>
 
 /**
- * struct Car - A structure representing a car in a race
- *
- * @id: the ID of the car
- * @laps: the number of laps completed by the car
+ * struct car - defines a new data type 'Car'
+ * @id: the id of the car
+ * @laps: the number of laps that the car has completed
+ * @next: a pointer to the next car in the linked list
  */
 
-typedef struct Car
+typedef struct car
 {
 	int id;
-	int laps;
+	size_t laps;
+	struct car *next;
 } Car;
 
-/**
- * struct Race - A structure representing a race
- *
- * @cars: an array of cars in the race
- * @num_cars: the number of cars in the race
- */
-
-typedef struct Race
-{
-	Car cars[MAX_CARS];
-	int num_cars;
-} Race;
-
-
-int find_car(int id);
-void add_car(int id, Race *race);
-void sort_cars(Race *race);
 void race_state(int *id, size_t size);
 
-
-#endif /* laps.h */
+#endif
