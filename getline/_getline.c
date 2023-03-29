3d73825d823079318ead2cbd3eecbdeb6bee3168
Razika-Bengana <5003@holbertonstudents.com>
2023-03-29 09:11:54 +0200

    function that reads an entire line from a file descriptor

--- getline/_getline.c
+++ getline/_getline.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include "_getline.h"
 
+// Define the READ_SIZE macro based on whether we want to count calls to read or not
+#ifdef COUNT_READ_CALLS
+#undef READ_SIZE
+#define READ_SIZE 1
+#endif
+
+// Define a macro to override malloc to initialize allocated memory to 0xFF
+#define malloc(size) memset(malloc(size), 0xFF, size)
+
 #define BUFFER_SIZE 1024
 
 char *_getline(const int fd)
@@ -10,6 +19,7 @@ char *_getline(const int fd)
 	static char buffer[BUFFER_SIZE];
 	static int pos;
 	static int size;
+	static int call_count;
 
 	char *line = NULL;
 	int i = pos;
@@ -21,10 +31,20 @@ char *_getline(const int fd)
 		{
 			pos = 0;
 			i = 0;
-			size = read(fd, buffer, BUFFER_SIZE);
+			size = read(fd, buffer, READ_SIZE);
+			call_count++;
+
 			if (size <= 0)
 			{
-/* If there's no more data to read, return NULL */
+				if (line != NULL && i > pos)
+				{
+					line = realloc(line, i - pos + 1);
+					memcpy(line, &buffer[pos], i - pos);
+					line[i - pos] = '\0';
+					pos = i;
+					return (line);
+				}
+				// Otherwise, return NULL
 				return (NULL);
 			}
 		}
@@ -52,7 +72,7 @@ char *_getline(const int fd)
  */
 		if (i == size)
 		{
-			line = malloc(i - pos + 1);
+			line = realloc(line, i - pos + 1);
 			if (line == NULL)
 			{
 				return (NULL);
