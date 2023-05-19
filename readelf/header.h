92a0f024a021a2bcf9efd508086e4fe17544eb78
Razika-Bengana <5003@holbertonstudents.com>
2023-05-19 06:08:24 +0200

    header file

--- readelf/header.h
+++ readelf/header.h
@@ -6,9 +6,33 @@
 #include <stdint.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 #include <elf.h>
 
-void print_elf_header(Elf64_Ehdr *elf_header);
-int main(int argc, char* argv[]);
+typedef struct FileInfo {
+    char *programName;
+    char *fileName;
+    int file_size;
+    _Bool elf_type;
+    _Bool is32bit;
+    _Bool isBigEndian;
+    FILE *stream;
+    Elf64_Ehdr header;
+} FileInfo;
+
+int main(int argc, char **argv);
+void free_memory(FileInfo *fileInfo);
+int get_elf_header(FileInfo *fileInfo);
+const char *get_elf_type(Elf64_Half e_type);
+const char *get_operating_system_abi(unsigned char osabi);
+void handle_errors(char *format, char *errorString, FileInfo *fileInfo);
+void init_file_info(FileInfo *fileInfo);
+int open_elf_file(FileInfo *fileInfo);
+int print_elf_header(FileInfo *fileInfo);
+const char *get_machine_name(Elf64_Half e_machine);
 
 #endif /* H_HEADER_H */
