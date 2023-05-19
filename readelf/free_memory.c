8654d38b75b92610b59f0a5ad6cb4e5437bbad99
Razika-Bengana <5003@holbertonstudents.com>
2023-05-19 06:06:30 +0200

    free the memory

---
+++ readelf/free_memory.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+/**
+ * free_memory - frees allocated memory
+ * @fileInfo: pointer to the struct
+ */
+void free_memory(FileInfo *fileInfo)
+{
+	if (fileInfo->programName != NULL)
+	{
+		fileInfo->programName = NULL;
+	}
+	if (fileInfo->fileName != NULL)
+	{
+		fileInfo->fileName = NULL;
+	}
+	if (fileInfo->elf_type != 0)
+	{
+		fileInfo->elf_type = 0;
+	}
+}
