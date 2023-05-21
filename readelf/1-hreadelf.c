4f8429f1a69b66bd5730cf0aed74c2a4408fa8a0
Razika-Bengana <5003@holbertonstudents.com>
2023-05-21 19:29:45 +0200

    correct betty style

--- readelf/1-hreadelf.c
+++ readelf/1-hreadelf.c
@@ -10,33 +10,33 @@
 
 int main(int argc, char **argv)
 {
-        FileInfo fileInfo;
-        int flag = 0;
+	FileInfo fileInfo;
+	int flag = 0;
 
-        if (argc != 2)
-        {
-                fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-                return (1);
-        }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (1);
+	}
 
-        init_file_info(&fileInfo);
-        fileInfo.programName = argv[0];
-        fileInfo.fileName = argv[1];
+	init_file_info(&fileInfo);
+	fileInfo.programName = argv[0];
+	fileInfo.fileName = argv[1];
 
-        flag = open_elf_file(&fileInfo);
-        if (flag == 0)
-        {
-                flag = get_elf_header(&fileInfo);
-                if (flag == 0)
-                {
-                        flag = print_elf_header(&fileInfo);
-                }
-                else
-                {
-                        handle_errors("%s: Failed to read file header\n", NULL, &fileInfo);
-                }
-        }
+	flag = open_elf_file(&fileInfo);
+	if (flag == 0)
+	{
+		flag = get_elf_header(&fileInfo);
+		if (flag == 0)
+		{
+			flag = print_elf_header(&fileInfo);
+		}
+		else
+		{
+			handle_errors("%s: Failed to read file header\n", NULL, &fileInfo);
+		}
+	}
 
-        free_memory(&fileInfo);
-        return (flag);
+	free_memory(&fileInfo);
+	return (flag);
 }
