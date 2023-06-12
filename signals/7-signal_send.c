1e96dec3e1908a594594d78fb21f7ccac1a4606b
Razika-Bengana <5003@holbertonstudents.com>
2023-06-12 05:38:24 +0200

    program that sends the signal SIGINT to a process, given its PID

---
+++ signals/7-signal_send.c
@@ -0,0 +1,30 @@
+#include "signals.h"
+
+/**
+ * main - entry point of the program
+ * @argc: number of command-line argument
+ * @argv: array of command-line argument strings
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+/*
+ * Convert the PID from string to integer using atoi
+ * and cast it to pid_t type
+ */
+	pid_t pid = (pid_t)atoi(argv[1]);
+
+/* Send the SIGINT signal to the specified process using kill */
+/* If the kill operation fails, return EXIT_FAILURE */
+	if (kill(pid, SIGINT) != 0)
+		return (EXIT_FAILURE);
+
+	return (EXIT_SUCCESS);
+}
