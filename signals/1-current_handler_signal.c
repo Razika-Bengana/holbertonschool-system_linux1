4c8b5f7ec69be831f7d6f35176d0ae736321a1a2
Razika-Bengana <5003@holbertonstudents.com>
2023-06-12 04:24:04 +0200

    function that retrieves the current handler of the signal SIGINT

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -8,26 +8,29 @@
  * back to the original handler to ensure the handler is unchanged after calling
  * this function.
  *
- * Return: Pointer to the current handler function for SIGINT, or NULL on failure.
+ * Return: Pointer to the current handler function for SIGINT,
+ * or NULL on failure.
  */
 
 void (*current_handler_signal(void))(int)
 {
 	sighandler_t handler;
 
-	/* Set the handler to SIG_IGN (ignore) temporarily to get the current handler */
+	/* Set the handler to SIG_IGN (ignore) temporarily
+	   to get the current handler */
 	handler = signal(SIGINT, SIG_IGN);
 
 	if (handler == SIG_ERR)
 	{
-		return NULL; /* Failed to set the signal handler */
+		return (NULL); /* Failed to set the signal handler */
 	}
 
 	/* Restore the original handler and return the current handler */
 	if (signal(SIGINT, handler) == SIG_ERR)
 	{
-		return NULL; /* Failed to set the signal handler back to the original handler */
+		return (NULL); /* Failed to set the signal handler
+				back to the original handler */
 	}
 
-	return handler;
+	return (handler);
 }
