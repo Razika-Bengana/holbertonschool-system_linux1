f5500181d04544b3d7b779c178f11dd98f4cd23c
Razika-Bengana <5003@holbertonstudents.com>
2023-06-14 19:56:00 +0200

    function that tests if a process exists, given its PID

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -10,22 +10,5 @@
 
 int pid_exist(pid_t pid)
 {
-
-/*
- *Sending a null signal (0) to the process to check its existence
- * without affecting it.
- * If the kill function returns 0, the process exists.
- */
-	if (kill(pid, 0) == 0)
-	{
-		return (1);
-	}
-	else
-	{
-/*
- * If the kill function returns -1, the process doesn't exist
- * or we don't have permission to send the signal.
- */
-		return (0);
-	}
+	return (kill(pid, 0) + 1);
 }
