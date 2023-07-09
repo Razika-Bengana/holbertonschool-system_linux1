79efc503394a865818d348beee6ee09f6b30ac02
Razika-Bengana <5003@holbertonstudents.com>
2023-07-09 21:21:02 +0200

    correct the main function

--- nm_objdump/hnm.c
+++ nm_objdump/hnm.c
@@ -1,91 +1,59 @@
 #include "hnm.h"
 
 /**
- * processFile - program that processes a single file using the hnm program
+ * main - entry point to hnm
  *
- * this function initializes the program state, sets the executable name and
- * the file name,then opens the ELF file;
- * if the file is opened successfully, it retrieves the file header;
- * if the file header is retrieved successfully, it prints the file name, and
- * retrieves also the section headers, symbol tables, and prints the symbol
- * tables;
- * finally, it closes the program state
+ * the main function processes each file specified as command-line arguments
+ * or processes the default file "a.out" if no arguments are provide;
+ * it iterates through the files, calls the necessary functions to retrieve and
+ * print information about each file, and returns the appropriate exit code
  *
- * @file_name: name of the file to process
- *
- * Return: 0 on success, or 1 on failure
- */
-
-int processFile(const char *file_name)
-{
-	nm_state state;
-
-	initState(&state);
-	state.exec_name = "hnm";
-	state.f_name = (char *)file_name;
-
-	if (openELF(&state) == 0)
-	{
-		if (getFileHeader(&state) == 0)
-		{
-			printf("\n%s:\n", file_name);
-			if (getSectionHeaders(&state) == 0 &&
-			    getSymTables(&state) == 0 &&
-			    printSymTables(&state) == 0)
-			{
-				closeState(&state);
-				return (0);
-			}
-		}
-
-		else
-		{
-			errorMsg("%s: File format not recognized\n", NULL,
-				 &state);
-		}
-	}
-
-	closeState(&state);
-	return (1);
-}
-
-/**
- * main - entry point to the hnm program
- *
- * the main function processes each file specified as command-line arguments,
- * or processes the default file "a.out" if no arguments are provided;
- * it calls the processFile function for each file and checks
- * the return values;
- *
- * @argc: command line argument count
- * @argv: command line argument array
+ * @argc: command line arg count
+ * @argv: command line arg array
  *
  * Return: 0 on success, 1 on failure
  */
 
 int main(int argc, char **argv)
 {
-	if (argc > 1)
+	char *default_args[] = {"a.out"};
+	char **args = argc > 1 ? argv + 1 : default_args;
+	nm_state state;
+	int i, retval, ac = argc > 1 ? argc - 1 : 1;
+/* nm takes multiple files as args */
+/* any failure for a single file sets exit code of 1 for entire loop */
+	for (i = 0, retval = 0; i < ac; i++)
 	{
-		for (int i = 1; i < argc; i++)
+		initState(&state);
+		state.exec_name = argv[0];
+		state.f_name = args[i];
+/* Open the ELF file */
+		if (openELF(&state) == 0)
 		{
-			if (processFile(argv[i]) != 0)
+/* Retrieve the file header */
+			if (getFileHeader(&state) == 0)
 			{
-/* Return failure if any file processing fails */
-				return (1);
+/* Print the file name if there are multiple files */
+				if (ac > 1)
+					printf("\n%s:\n", state.f_name);
+/* Retrieve and print section headers, symbol tables, and symbols */
+				if (!(getSectionHeaders(&state) == 0 &&
+				      getSymTables(&state) == 0 &&
+				      printSymTables(&state) == 0))
+/* Set exit code to 1 if any file processing fails */
+					retval = 1;
+			}
+			else
+			{
+				errorMsg("%s: File format not recognized\n",
+					 NULL, &state);
+				retval = 1;
 			}
 		}
+		else
+			retval = 1; /* Failed to open the file */
+		closeState(&state); /* Close the program state */
 	}
 
-	else
-	{
-/* No files provided, process the default file */
-		if (processFile("a.out") != 0)
-		{
-/* Return failure if default file processing fails */
-			return (1);
-		}
-	}
-/* Return success if all files were processed successfully */
-	return (0);
+	return (retval);
 }
